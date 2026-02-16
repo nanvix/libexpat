79a843dd9fdb0e859a35f5f2153bc3ae8ee0f01f
Pedro Henrique Penna <ppenna@microsoft.com>
2026-02-16 13:57:25 -0800

    feat(nanvix): add expat_config.h for cross-compilation
    Add a pre-configured expat_config.h suitable for Nanvix and other
    POSIX-compatible targets. This avoids requiring autotools/CMake to
    generate the configuration header during cross-compilation.
    
    Also update .gitignore to exclude build artifacts (*.o, *.a) while
    tracking expat_config.h.

---
+++ expat/expat_config.h
@@ -0,0 +1,24 @@
+#define BYTEORDER 1234
+#define HAVE_BCOPY 1
+#define HAVE_DLFCN_H 1
+#define HAVE_FCNTL_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_MEMMOVE 1
+#define HAVE_MEMORY_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_STRING_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_UNISTD_H 1
+#define PACKAGE_BUGREPORT "expat-bugs@libexpat.org"
+#define PACKAGE_NAME "expat"
+#define PACKAGE_STRING "expat 2.6.4"
+#define PACKAGE_TARNAME "expat"
+#define PACKAGE_URL ""
+#define PACKAGE_VERSION "2.6.4"
+#define STDC_HEADERS 1
+#define XML_CONTEXT_BYTES 1024
+#define XML_DTD 1
+#define XML_NS 1
